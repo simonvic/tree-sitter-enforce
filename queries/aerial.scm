815e7852fa78b192fb858c3a3303bc90d1e3b7b9
simonvic <simonvic.dev@gmail.com>
2024-11-11 22:31:19 +0100

    feat: update aerial queries

--- queries/aerial.scm
+++ queries/aerial.scm
@@ -1,20 +1,63 @@
-(declMethod
-  name: (identifier) @name @start
-  (#set! "kind" "Method")) @symbol
-
-(declDeconstructor
-  (identifier) @name
-  (#set! "kind" "Constructor")) @symbol
-
-(declClass
-  typename: (identifier) @name
-  (#set! "kind" "Class")) @symbol
+; enums
 
 (declEnum
   typename: (identifier) @name
-  (#set! "kind" "Enum")) @symbol
+  (#set! "kind" "Enum")
+  ) @symbol
+
+(enumMember
+  name: (identifier) @name
+  (#set! "kind" "EnumMember")
+  ) @symbol
+
+; class
+
+(declClass
+  typename: (identifier) @name
+  (#set! "kind" "Class")
+  ) @symbol
+
+(typedef
+  alias: (identifier) @name
+  (#set! "kind" "Class")
+  ) @symbol
+
+; constructor
+(declClass
+  typename: (identifier) @_typename
+  body: (classBody
+    (declMethod
+	    name: (identifier) @name
+	    (#eq? @name @_typename)
+	    (#set! "kind" "Constructor")
+      ) @symbol
+    )
+  )
+
+; deconstructor
+
+(declDeconstructor
+  (identifier) @name
+  (#set! "kind" "Constructor")
+  ) @symbol
+
+; fields
 
 (declField
-  (identifier) @name
-  (#set! "kind" "Field")) @symbol
+  ((fieldModifier) @_modifier (#eq? @_modifier "const"))
+  type: (_)
+  name: (identifier) @name
+  (#set! "kind" "Constant")
+  ) @symbol
 
+(declField
+  type: (_)
+  name: (identifier) @name
+  (#set! "kind" "Field")
+  ) @symbol
+
+; methods
+(declMethod
+  name: (identifier) @name
+  (#set! "kind" "Method")
+  ) @symbol
