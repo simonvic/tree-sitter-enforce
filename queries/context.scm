40f9cd2ca641e28b1e4334a7e15a3291581136fc
simonvic <simonvic.dev@gmail.com>
2024-11-11 20:49:42 +0100

    feat: add context queries

---
+++ queries/context.scm
@@ -0,0 +1,33 @@
+(if
+  (block) @context.end
+  ) @context
+
+(declMethod
+  body: (_) @context.end
+  ) @context
+
+(for
+  body: (_) @context.end
+  ) @context
+
+(foreach
+  body: (_) @context.end
+  ) @context
+
+(declClass
+  body: (_) @context.end
+  ) @context
+
+(switch
+  body: (_) @context.end
+  ) @context
+
+(switch
+  body: (_) @context.end
+  ) @context
+
+(switchCase
+  label: (_) @context.end
+  ) @context
+
+(statementExpression) @context
